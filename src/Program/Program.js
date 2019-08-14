6d23984680360bc5f5ac3003862123591292c7fb
Luis Santiago <luis.santiagoayala@gmail.com>
2019-08-13 22:16:43 -0400

    Modify create workout form.

--- src/Program/Program.js
+++ src/Program/Program.js
@@ -4,6 +4,10 @@ import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -12,11 +16,15 @@ const useStyles = makeStyles(theme => ({
       marginBottom: '2.0em'
     },
     formControl: {
-      margin: theme.spacing(1),
+      margin: theme.spacing(2),
       minWidth: 120,
     },
-    selectEmpty: {
-      marginTop: theme.spacing(2),
+    button: {
+      marginTop: theme.spacing(3),
+      marginRight: "inherit"
+    },
+    formButton: {
+        marginBottom: '3.0em'
     },
     typography: {
         fontWeight: "bold",
@@ -30,18 +38,27 @@ const Program = (props) => {
     <div id="program" className="program">
         <h2>Create New Workout:</h2>
         <form className="main-form" autoComplete="off">
-            //TODO: Separate the containers below into their own Components.
+            {/* TODO:Separate the containers below into their own Components. */}
             <Typography variant="h6" gutterBottom className={classes.typography}>
                 Format:
             </Typography>
             <Grid container className={classes.root} spacing={2}>
                 <Grid item xs={12} sm={4}>
-                    <TextField
-                        id="type"
-                        label="Workout Type"
-                        placeholder="Workout Type"
-                        margin="normal"
-                    />
+                    <FormControl className={classes.formControl}>
+                        <InputLabel htmlFor="type">Workout Type</InputLabel>
+                        <Select
+                        //value={values.age}
+                        //onChange={handleChange}
+                        inputProps={{
+                            name: 'type',
+                            id: 'type',
+                        }}
+                        >
+                        <MenuItem value={10}>For Time</MenuItem>
+                        <MenuItem value={20}>AMARAP</MenuItem>
+                        <MenuItem value={30}>EMOM</MenuItem>
+                        </Select>
+                    </FormControl>
                 </Grid>
                 <Grid item xs={12} sm={4}>
                     <TextField
@@ -81,6 +98,11 @@ const Program = (props) => {
                         margin="normal"
                     />
                 </Grid>
+                <Grid item xs={12} sm={1}>
+                    <Button variant="contained" className={classes.button}>
+                        ADD
+                    </Button>
+                </Grid>
             </Grid>
 
             <Typography variant="h6" gutterBottom className={classes.typography}>
@@ -96,21 +118,36 @@ const Program = (props) => {
                     />
                 </Grid>
                 <Grid item xs={12} sm={4}>
-                    <TextField
-                        id="equipment"
-                        label="Equipment"
-                        placeholder="Equipment"
-                        margin="normal"
-                    />
+                <FormControl className={classes.formControl}>
+                        <InputLabel htmlFor="gender">Gender</InputLabel>
+                        <Select
+                        //value={values.age}
+                        //onChange={handleChange}
+                        inputProps={{
+                            name: 'gender',
+                            id: 'gender',
+                        }}
+                        >
+                        <MenuItem value={10}>Male</MenuItem>
+                        <MenuItem value={20}>Female</MenuItem>
+                        </Select>
+                    </FormControl>
                 </Grid>
-                <Grid item xs={12} sm={4}>
-                    <TextField
-                        id="gender"
-                        label="Gender"
-                        placeholder="Gender"
-                        margin="normal"
-                    />
+                <Grid item xs={12} sm={1}>
+                    <Button variant="contained" className={classes.button}>
+                        ADD
+                    </Button>
                 </Grid>
+            </Grid>
+            <Grid className={classes.formButton} item xs={12}>
+                <Button
+                    variant="contained"
+                    color="primary"
+                    //onClick={}
+                    className={classes.button}
+                  >
+                    Save
+                </Button>
             </Grid>
         </form>
     </div>
